eedb93be793b75c403c530c912bec1cc428b5f14
Katherine Walendzik <katherinewalendzik@gmail.com>
2019-12-12 03:57:45 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.map{|name| badge_maker(name)}
+end
+
+def assign_rooms(attendees)
+  attendees.map.each_with_index{|name, index| "Hello, #{name}! You'll be assigned to room #{index + 1}!"}
+end
+
+def printer(attendees)
+  attendees.each_with_index do |name, index|
+  puts badge_maker(name)
+  puts 
+  end
+end
