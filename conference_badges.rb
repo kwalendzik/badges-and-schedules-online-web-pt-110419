16808e0c57f3cd784074c5608a5fbe8fa789c17d
Katherine Walendzik <katherinewalendzik@gmail.com>
2019-12-12 04:02:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,7 +12,7 @@ end
 
 def printer(attendees)
   attendees.each do |name, index|
-  puts batch_badge_creator(attendees)
-  puts assign_rooms(attendees)
+  puts batch_badge_creator(attendees).join
+  puts assign_rooms(attendees).join
   end
 end
